7450361863a4d74dce38b88e71e40ecb02ecf12d
sally0y <fgsfsd133@gmail.com>
2022-10-17 11:23:55 +0900

    first commit

---
+++ src/main/java/lecture/p01lombok/App.java
@@ -0,0 +1,7 @@
+package lecture.p01lombok;
+
+public class App {
+	public static void main(String[] args) {
+		System.out.println("메인 메소드");
+	}
+}
---
+++ src/main/java/lecture/p01lombok/JavaBean5.java
@@ -0,0 +1,14 @@
+package lecture.p01lombok;
+
+import lombok.Data;
+
+@Data //아래 다섯개의 어노테이션을 한 번에 적용함
+//@Tostring
+//@Getter
+//@Setter
+//@AllArgsConstructor
+
+public class JavaBean5 {
+	private String name;
+	private int age;
+}
